94c21d256281e5d362d839ee5633a32efc24a25f
sayaurelia <sayaurellia@gmail.com>
2025-02-24 14:12:04 +0700

    Add files via upload

---
+++ TipeData.js
@@ -0,0 +1,7 @@
+let nama = "Syaifana"; // string
+let umur = 16;         // number
+let isStudent = true;  // boolean
+let alamat;            // undefined
+let nilai = null;      // null
+
+console.log(typeof nama); // output: string
---
+++ promptalert.js
@@ -0,0 +1,10 @@
+// Alert: Menampilkan pesan
+alert("Selamat datang di website kami!");
+
+// Prompt: Meminta input dari pengguna
+let nama = prompt("Masukkan nama Anda:");
+console.log("Nama Anda adalah " + nama);
+
+// Confirm: Memberikan pilihan OK atau Cancel
+let yakin = confirm("Apakah Anda yakin ingin keluar?");
+console.log(yakin); // Output: true jika OK, false jika Cancel
