e169425073aa01cab17178e4ba6b2ad2256783c1
sayaurelia <sayaurellia@gmail.com>
2025-02-24 14:14:05 +0700

    Add files via upload

---
+++ javascript/operator.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 5;
+console.log(a + b); 
+console.log(a - b);
+console.log(a * b);
+console.log(a / b); 
+console.log(a % b); 
+
+let firstName = "Syaifana";
+let lastName = "Aurelia";
+console.log(firstName + " " + lastName); 
---
+++ javascript/operator2.js
@@ -0,0 +1,9 @@
+console.log(10 > 5);  
+console.log(10 < 5);  
+console.log(10 == "10"); 
+console.log(10 === "10"); 
+console.log(10 !== 5);
+
+console.log(true && false); 
+console.log(true || false); 
+console.log(!true); 
