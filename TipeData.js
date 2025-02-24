3560f0c3bb0cfe8e9f398ecb99d9d29bf8daed18
sayaurelia <sayaurellia@gmail.com>
2025-02-24 14:22:33 +0700

    Delete TipeData.js

--- TipeData.js
+++
@@ -1,7 +0,0 @@
-let nama = "Syaifana"; // string
-let umur = 16;         // number
-let isStudent = true;  // boolean
-let alamat;            // undefined
-let nilai = null;      // null
-
-console.log(typeof nama); // output: string
